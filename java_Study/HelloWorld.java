1564150bd7ad969f7ffe0af52b45264c88984d02
mineruwo <fullstack.mineruwo@gmail.com>
2025-05-12 17:44:21 +0900

    init comiits

---
+++ java_Study/HelloWorld.java
@@ -0,0 +1,14 @@
+package java_Study;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("안녕 난 손민우야");
+	
+//문자셋 설정 : 이 설정이 각각 틀리면, 한글 데이터나 ,주석이 깨줄 수 가 있음
+//때문에 반드시 문제셋을 설정을 초기에 해 둬야 나중에 후회하지 않음
+		
+	}
+
+}
